e6d75a24e60f80dfde62112999b33b1858347950
chandrashekhargoka <chandrashekhar.goka@gmail.com>
2019-06-02 14:17:41 +0530

    initial commit

---
+++ src/main/java/com/onlinetutorialspoint/SpringBootThymeLeafExampleApplication.java
@@ -0,0 +1,13 @@
+package com.onlinetutorialspoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeLeafExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeLeafExampleApplication.class, args);
+	}
+
+}
